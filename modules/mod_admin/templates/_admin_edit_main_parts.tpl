mawuli-ypa/zotonic

commit 9ce2f10c73065f6b695d254495e5d47de1b8276d

mod_admin: Easier admin edit page customization
Move blocks in the admin edit page to 2 separate template files, so
that the templates can be overridden by category type. Documentation
included.

Squashed commit of the following:

commit 4a68aaa89ba4c0a02e2feef866cdfd9a4c099af2
Author: Arjan Scherpenisse <arjan@scherpenisse.net>
Date:   Sun Jul 7 11:21:07 2013 +0200

    Tweaks for admin edit page customsations

commit c033a967e1d00c231965e60d230bb1f8460b198a
Author: Arthur Clemens <arthur@visiblearea.com>
Date:   Sat Jul 6 17:11:05 2013 +0200

    Enhance documentation

    Add a note about meta to show elements on category pages but not on
    instances of that category.

commit 4af0ab38f42576287a052ffb767a8d83197e72bd
Author: Arthur Clemens <arthur@visiblearea.com>
Date:   Sat Jul 6 16:42:53 2013 +0200

    Add reference to r

    Needs reference to m.rsc[id]

commit 2bffa9d2b683edf111f684a5a26ac32184334a49
Author: Arthur Clemens <arthur@visiblearea.com>
Date:   Sat Jul 6 14:41:22 2013 +0200

    Easier admin customization

    Move blocks in the admin edit page to 2 separate template files, so
    that the templates can be overridden by category type. Documentation
    included.

modules/mod_admin/templates/_admin_edit_main_parts.tpl
@@ -1,0 +1,21 @@
+{% block admin_edit_form_top %}{% endblock %}
+
+{% all catinclude "_admin_edit_basics.tpl" id is_editable=is_editable languages=languages %}
+{% all catinclude "_admin_edit_content.tpl" id is_editable=is_editable languages=languages %}
+
+{% if id.category_id.feature_show_address|if_undefined:`true` %}
+    {% catinclude "_admin_edit_content_address.tpl" id is_editable=is_editable languages=languages %}
+{% endif %}
+
+{% if r.is_a.media or r.medium %}
+    {% include "_admin_edit_content_media.tpl" %}
+{% endif %}
+
+{% catinclude "_admin_edit_body.tpl" id is_editable=is_editable languages=languages %}
+{% catinclude "_admin_edit_blocks.tpl" id is_editable=is_editable languages=languages %}
+{% catinclude "_admin_edit_depiction.tpl" id is_editable=is_editable languages=languages %}
+
+{# {% catinclude "_admin_edit_haspart.tpl" id is_editable=is_editable languages=languages %} #}
+
+{% include "_admin_edit_content_advanced.tpl" %}
+{% include "_admin_edit_content_seo.tpl" %}

modules/mod_admin/templates/admin_edit.tpl
@@ -47,50 +47,11 @@
 
 	<div class="row-fluid">
 		<div class="span8" id="poststuff">
-			{% block admin_edit_form_top %}{% endblock %}
-
-			{% all catinclude "_admin_edit_basics.tpl" id is_editable=is_editable languages=languages %}
-			{% all catinclude "_admin_edit_content.tpl" id is_editable=is_editable languages=languages %}
-
-	        {% if id.category_id.feature_show_address|if_undefined:`true` %}
-	        	{% catinclude "_admin_edit_content_address.tpl" id is_editable=is_editable languages=languages %}
-	        {% endif %}
-	        
-			{% if r.is_a.media or r.medium %}
-				{% include "_admin_edit_content_media.tpl" %}
-			{% endif %}
-
-			{% catinclude "_admin_edit_body.tpl" id is_editable=is_editable languages=languages %}
-			{% catinclude "_admin_edit_blocks.tpl" id is_editable=is_editable languages=languages %}
-			{% catinclude "_admin_edit_depiction.tpl" id is_editable=is_editable languages=languages %}
-
-			{# {% catinclude "_admin_edit_haspart.tpl" id is_editable=is_editable languages=languages %} #}
-
-			{% include "_admin_edit_content_advanced.tpl" %}
-			{% include "_admin_edit_content_seo.tpl" %}
+			{% catinclude "_admin_edit_main_parts.tpl" id is_editable=is_editable languages=languages r=r %}
 		</div>
 
 		<div class="span4" id="sidebar">
-			{% include "_admin_edit_footer.tpl" %}
-
-			<div id="sort"> {# also sidebar #}
-			{% include "_admin_edit_content_publish.tpl" headline="simple" %}
-
-	        {% if r.is_a.meta %}
-				{% include "_admin_edit_meta_features.tpl" %}
-	        {% endif %}
-	        
-			{% include "_admin_edit_content_acl.tpl" %}
-
-			{% if not r.is_a.meta %}
-				{% include "_admin_edit_content_pub_period.tpl" %}
-				{% include "_admin_edit_content_date_range.tpl" %}
-			{% endif %}
-
-			{% all catinclude "_admin_edit_sidebar.tpl" id languages=languages %}
-
-			{% include "_admin_edit_content_page_connections.tpl" %}
-			</div>
+			{% catinclude "_admin_edit_sidebar_parts.tpl" id is_editable=is_editable languages=languages r=r %}
 		</div>
 	</div>
 </form>

modules/mod_admin/templates/_admin_edit_sidebar_parts.tpl
@@ -1,0 +1,20 @@
+{% include "_admin_edit_floating_buttons.tpl" %}
+
+<div id="sort"> {# also sidebar #}
+    {% include "_admin_edit_content_publish.tpl" headline="simple" %}
+
+    {% if r.is_a.meta %}
+        {% include "_admin_edit_meta_features.tpl" %}
+    {% endif %}
+
+    {% include "_admin_edit_content_acl.tpl" %}
+
+    {% if not r.is_a.meta %}
+        {% include "_admin_edit_content_pub_period.tpl" %}
+        {% include "_admin_edit_content_date_range.tpl" %}
+    {% endif %}
+
+    {% all catinclude "_admin_edit_sidebar.tpl" id languages=languages %}
+
+    {% include "_admin_edit_content_page_connections.tpl" %}
+</div>

commit 3bccd8d835223f70f9428e7d0b2d39e8f394eb69

mod_admin: admin_edit_form_top block needs to be in admin_edit.tpl
Bug introduced in 9ce2f10c73065f6b695d254495e5d47de1b8276d; the menu
editor did not display the menu anymore.

modules/mod_admin/templates/_admin_edit_main_parts.tpl
@@ -1,4 +1,3 @@
-{% block admin_edit_form_top %}{% endblock %}
 
 {% all catinclude "_admin_edit_basics.tpl" id is_editable=is_editable languages=languages %}
 {% all catinclude "_admin_edit_content.tpl" id is_editable=is_editable languages=languages %}

modules/mod_admin/templates/admin_edit.tpl
@@ -47,6 +47,7 @@
 
 	<div class="row-fluid">
 		<div class="span8" id="poststuff">
+            {% block admin_edit_form_top %}{% endblock %}
 			{% catinclude "_admin_edit_main_parts.tpl" id is_editable=is_editable languages=languages r=r %}
 		</div>
 