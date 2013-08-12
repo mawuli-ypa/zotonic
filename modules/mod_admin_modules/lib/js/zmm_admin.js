d4a026473c64c017bc6c75ec981e6ab3576b4be0
Mawuli Adzaku <mawuli@mawuli.me>
2013-08-12 04:27:20 +0000

    mod_admin_modules: Cleanup code
    Rename all functions, and html elements to confrom to Zotonic's naming convention.
    Other changes include:
    
    * Change table row background colour when clicked. This is to indicate that is has been selected
    * When an already highlighted row is clicked, remove module from list of modules to install
    * Dsiplay a helpful message while the module list are fetched from the server
    * Do not display growl message after fetching the latest modules from the server
    * The ZMM table is no longer striped, as it was interfering with the highlighting of tables
    * Add "install" button when a row is clicked/selected. This is only done when the "install" button is not visible

--- modules/mod_admin_modules/lib/js/zmm_admin.js
+++ modules/mod_admin_modules/lib/js/zmm_admin.js
@@ -1,51 +1,115 @@
-function populate_zmr_table(){
+// global module manager config
+Window.zmm = Window.zmm || {}
+
+// function for the module manager
+function z_populate_zmm_table(){
         /*
-         * todo: get zmr server from Zotonic node, 
-         * cache results 
-         * test in other browsers, especially mobile
-         * install modules: dynamic postback to server
-         * refresh data
-         * make zmr results table scrollable
+         * Todo: get zmr server from Zotonic node? 
+         * Cache results 
+         * Test in other browsers, especially mobile
+         * Install modules: dynamic postback to server
+         * Make zmr results table scrollable
+         * Add info help button
          */
+        $("#zmm_tbl_body").empty().html("Fetching modules...");
         var ZMR = "http://modules.zotonic.com/api/zmr/repositories?callback=?"
         $.getJSON(ZMR, {},function(data){
-            Window.zmr_data = data;
-            //console.log(Window.zmr_data);
-
-        $.each(Window.zmr_data,function(index, data){
+        $("#zmm_tbl_body").empty();
+        $.each(data,function(index, data){
            columns = "<td>" + data.title + "</td>" + "<td>" + data.repository + "</td>";       
-           $("#zmr_tbl_body").append("<tr>" + columns + "</tr>");
-           //console.log(data);
+           $("#zmm_tbl_body").append("<tr class='z_module_link' data-id='" + data.title + "'" + 
+                                      "data-repo='" + data.repository + "'" + ">" + columns + "</tr>");
          });
       });
 }
 
-function search_zmr(){
-  $('input#zmr_filter').live('keyup', function() {
+
+function z_filter_zmm_table(){
+  $('input#zmm_filter').live('keyup', function() {
     var rex = new RegExp($(this).val(), 'i');
-    $('#zmr_tbl tr').hide();
-        $('#zmr_tbl tr').filter(function() {
+    $('#zmm_tbl tr').hide();
+        $('#zmm_tbl tr').filter(function() {
             return rex.test($(this).text());
         }).show();
     });
  }
 
 
-function zmm_refresh(){
+function z_refresh_zmm(){
     z_growl_add("Fetching the latest the modules...");
-    $("#zmr_tbl_body").empty();
-    populate_zmr_table();
-    z_growl_add("Finished updating module list");
+    $("#zmm_tbl_body").empty();
+    z_populate_zmm_table();
+}
+
+function z_install_module(module){
+         // triger postback notify event on the server
+         z_notify('install_module', { 
+             z_delegate: 'mod_admin_modules', 
+             title: module.title,
+             repository: module.repository
+     });
+}
+
+function z_zmm_queue_install(module){
+    // add to list if it's not already added
+    Window.zmm.to_install = Window.zmm.to_install || [];
+
+    if($.grep(Window.zmm.to_install, function(m, i){
+         return m.title == module.title;
+    }) == false ){ Window.zmm.to_install.push(module)}
+}
+
+function z_zmm_unqueue_install(title){
+    Window.zmm.to_install = $.grep(Window.zmm.to_install,
+                                   function(m ,i){return m.title != title});
 }
 
 // Populate table with data from ZMR server
 // Also filter user input
-populate_zmr_table();
-search_zmr();
+z_populate_zmm_table();
+z_filter_zmm_table();
 
 
-
+// CALLBACKS / EVENT LISTENERS
 //attach event to the refresh button
-$("button#zmr_refresh_btn").live('click', function(e){
-    zmm_refresh();
+$("button#zmm_refresh_btn").live('click', function(e){
+    z_refresh_zmm();
+    e.preventDefault();
+});
+
+
+$(".z_module_link").live('click', function(e){
+    // mark module for installation
+    $(this).toggleClass('to_install');
+    
+    // display install button if it's hidden
+    if($("#zmm_install_btn").not(":visible")){
+        $("#zmm_install_btn").show();
+    }
+    title = $(this).attr('data-id');
+
+    // click highlight items to remove from install queue
+    if($(this).hasClass("highlight")){
+       return  z_zmm_unqueue_install(title);
+    } 
+    repository = $(this).attr('data-repo');
+    module = {title:title, repository: repository};
+
+    // add module to list of modules to install
+    z_zmm_queue_install(module);
+    e.preventDefault();
+
+});
+
+
+$("#zmm_install_btn").live('click', function(e){
+    modules = $.map(Window.zmm.to_install, function(m, i){module.title});
+    console.log(modules);
+    install_confirm_text = "<h3>These modules will be installed: </h3> <br />" + modules
+    z_dialog_confirm({
+        text: text,
+        on_confirm: function(){
+            $.each(Window.zmm.to_install, function(index, module){z_install_module(module)})
+    }})
+   e.preventDefault();
 });
